11d222703d27c40cb482726151aa87119a3b882a
JesseDaGriffin <43197176+JesseDaGriffin@users.noreply.github.com>
2018-12-02 16:09:57 -0800

    Change page index

--- App/logBook/imports/ui/layouts/index.js
+++ App/logBook/imports/ui/layouts/index.js
@@ -1,4 +1,8 @@
-import '/body';
-import '/footer';
-import '/header';
-import '/if-logged-in';
+import './body/body.html';
+import './body/body.js';
+import './footer/Footer.html';
+import './footer/Footer.html';
+import './header/Header.html';
+import './header/Header.js';
+import './if-logged-in/if-logged-in.html';
+import './if-logged-in/if-logged-in.js';
